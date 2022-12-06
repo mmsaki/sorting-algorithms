4f0c66c3f6314b7698f2a8beea7355f74bfb90bd
8888 <alicehume@msakieth.local>
2022-12-06 05:42:39 -0600

    adding sorting notebook

---
+++ sorting.py
@@ -0,0 +1,32 @@
+"""
+Soring algorithms as read in https://realpython.com/sorting-algorithms-python/
+
+1. The Bubble Sort Algorithm in Python
+- Implementing Bubble Sort in Python
+- Measuring Bubble Sort’s Big O Runtime Complexity
+- Timing Your Bubble Sort Implementation
+- Analyzing the Strengths and Weaknesses of Bubble Sort
+2. The Insertion Sort Algorithm in Python
+- Implementing Insertion Sort in Python
+- Measuring Insertion Sort’s Big O Runtime Complexity
+- Timing Your Insertion Sort Implementation
+- Analyzing the Strengths and Weaknesses of Insertion Sort
+3. The Merge Sort Algorithm in Python
+- Implementing Merge Sort in Python
+- Measuring Merge Sort’s Big O Complexity
+- Timing Your Merge Sort Implementation
+- Analyzing the Strengths and Weaknesses of Merge Sort
+4. The Quicksort Algorithm in Python
+- Implementing Quicksort in Python
+- Selecting the pivot Element
+- Measuring Quicksort’s Big O Complexity
+- Timing Your Quicksort Implementation
+- Analyzing the Strengths and Weaknesses of Quicksort
+5. The Timsort Algorithm in Python
+- Implementing Timsort in Python
+- Measuring Timsort’s Big O Complexity
+- Timing Your Timsort Implementation
+- Analyzing the Strengths and Weaknesses of Timsort
+- Conclusion
+"""
+
